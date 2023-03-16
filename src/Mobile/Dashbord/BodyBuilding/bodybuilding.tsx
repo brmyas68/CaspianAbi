3b50df61d74b1b9d88b975e65083532174d8d6bd
@mohamad66 <rahimi@homacall.com>
2023-03-17 00:25:44 +0330

    [p

--- src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
+++ src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
@@ -11,13 +11,13 @@ const  BodyBuilding =() =>{
 
            <div style={{marginRight:'2%' , marginLeft:'2%' }}>
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-                <Col span={7}  style={{ textAlign:'center' , marginTop:'11px', fontWeight:'600', }}>
+                <Col span={7}  style={{ textAlign:'center' , marginTop:'15px', fontWeight:'600', }}>
                   12 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'11px', fontWeight:'600', }}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px', fontWeight:'600', }}>
                  400,000 تومان
                 </Col>
-                <Col  span={8}  style={{ textAlign:'center' , marginTop:'7px',  }}>
+                <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
                  <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,fontWeight:'600', background:'green' ,   fontSize:'12px'}}>
                      خرید اشتراک
                 </Button>
@@ -25,13 +25,13 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'11px', fontWeight:'600', }}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px', fontWeight:'600', }}>
                   16 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'11px', fontWeight:'600', }}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px', fontWeight:'600', }}>
                  500,000 تومان
                 </Col>
-                <Col  span={8}  style={{ textAlign:'center' , marginTop:'7px',  }}>
+                <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
                  <Button type="primary"    style={{ height:'30px' ,   width:'80%' , fontWeight:'600',background:'green' ,   fontSize:'12px'}}>
                      خرید اشتراک
                 </Button>
@@ -39,13 +39,13 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'11px', fontWeight:'600', }}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px', fontWeight:'600', }}>
                   20 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'11px',  fontWeight:'600',}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontWeight:'600',}}>
                  600,000 تومان
                 </Col>
-                <Col  span={8}  style={{ textAlign:'center' , marginTop:'7px',  }}>
+                <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
                  <Button type="primary"    style={{ height:'30px' ,   width:'80%' , fontWeight:'600',background:'green' ,   fontSize:'12px'}}>
                      خرید اشتراک
                 </Button>
@@ -53,13 +53,13 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'11px',  fontWeight:'600',}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontWeight:'600',}}>
                   30 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'11px',  fontWeight:'600',}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontWeight:'600',}}>
                  800,000 تومان
                 </Col>
-                <Col  span={8}  style={{ textAlign:'center' , marginTop:'7px',  }}>
+                <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
                  <Button type="primary"    style={{ height:'30px' ,   width:'80' , fontWeight:'600',background:'green' ,   fontSize:'12px'}}>
                      خرید اشتراک
                 </Button>
