c48ab88c9d6c263f408ffddd89659c99efc36560
@mohamad66 <rahimi@homacall.com>
2023-03-23 23:13:32 +0430

    rre

--- src/Mobile/Dashbord/Masaj/masaj.tsx
+++ src/Mobile/Dashbord/Masaj/masaj.tsx
@@ -7,6 +7,9 @@ import HeaderMsaj from "../Header/HeaderMasaj";
  import './masaj.css';
  import { Navigate, useNavigate } from 'react-router-dom';
 
+ import './../../fonts/irsans.ttf';
+import './../dashbord.css';
+
 const Masaj =() => {
    
      let navigate = useNavigate();
@@ -20,13 +23,13 @@ const Masaj =() => {
         <HeaderMsaj  /> 
          
          
-        <div style={{width:'90%', height:'100vh' , marginTop:'90px' , marginRight:'5%', marginLeft:'5%' , justifyContent:'start' , alignContent:'start' , flexDirection:'column'  ,    display:'flex'}}>
+        <div style={{width:'90%', height:'100vh' , marginTop:'120px' , marginRight:'5%', marginLeft:'5%' , justifyContent:'start' , alignContent:'start' , flexDirection:'column'  ,    display:'flex'}}>
              
-         <Button type="dashed" style={{height:'50px', fontSize:'15px', fontWeight:'600', fontFamily:'tahoma'}} shape="default" onClick={F_MasajBodyBuilding} >
+         <Button type="dashed" style={{height:'50px', fontSize:'15px', fontFamily:'irsans',}} shape="default" onClick={F_MasajBodyBuilding} >
            ماساژ بدنسازی
          </Button>
             <br />
-         <Button type="dashed" style={{height:'50px', fontSize:'15px', fontWeight:'600', fontFamily:'tahoma'}} shape="default">
+         <Button type="dashed" style={{height:'50px', fontSize:'15px', fontFamily:'irsans',}} shape="default">
            ماساژ استخر
          </Button>
          <br />
--- src/Mobile/Dashbord/Masaj/masajBodyBuilding.tsx
+++ src/Mobile/Dashbord/Masaj/masajBodyBuilding.tsx
@@ -4,7 +4,8 @@ import HeaderMasajBodyBuilding from "../Header/HeaderMasajBodyBuilding";
 import { Button,  Card,  Col,  Image, Row, Tooltip } from 'antd';
  
  
- 
+import './../../fonts/irsans.ttf';
+import './../dashbord.css';
  
 const MasajBodyBuilding =() => {
    
@@ -28,28 +29,28 @@ const MasajBodyBuilding =() => {
                     }}
                 >
                  <Row justify="start" dir="rtl">
-                    <Col span={24}  style={{ textAlign:'center' ,  fontSize:'15px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'15px', paddingRight:'0px'  }}>
+                    <Col span={24}  style={{ fontFamily:'irsans', textAlign:'center' ,  fontSize:'13px' , color:'#170602' ,   marginTop:'15px', paddingRight:'0px'  }}>
                         ماساژ تایلندی کل بدن  
                     </Col>
                  </Row>
                  <br />
                  <Row justify="start" dir="rtl">
-                    <Col span={7}  style={{ textAlign:'center' ,  fontSize:'13px' , color:'#bab4b5' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={7}  style={{ fontFamily:'irsans', textAlign:'center' ,  fontSize:'12px' , color:'#bab4b5' ,   marginTop:'10px', paddingRight:'0px'  }}>
                       مدت زمان ماساژ:   
                     </Col>
-                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'13px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={5}  style={{ fontFamily:'irsans', textAlign:'right' ,  fontSize:'12px' , color:'#170602' ,    marginTop:'10px', paddingRight:'0px'  }}>
                         45 دقیقه
                     </Col>
-                    <Col span={6}  style={{ textAlign:'center' ,  fontSize:'13px' , color:'#bab4b5' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={6}  style={{ fontFamily:'irsans', textAlign:'center' ,  fontSize:'12px' , color:'#bab4b5' ,   marginTop:'10px', paddingRight:'0px'  }}>
                         هزینه ماساژ :
                     </Col>
-                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'13px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={5}  style={{ fontFamily:'irsans', textAlign:'right' ,  fontSize:'12px' , color:'#170602' ,    marginTop:'10px', paddingRight:'0px'  }}>
                         45000 تومان  
                     </Col>
                  </Row>
                 <Row>
                         <Col  span={24}  style={{ textAlign:'center' , marginTop:'20px', marginBottom:'15px' }}>
-                            <Button type="primary" block   style={{  fontSize:'15px' , fontWeight:'600', height:'35px' ,   width:'90%' , background:'green' ,    }}>
+                            <Button type="primary" block   style={{fontFamily:'irsans',  fontSize:'12px' ,   height:'35px' ,   width:'90%' , background:'green' ,    }}>
                                 رزرو  ماساژ
                             </Button>
                         </Col>
@@ -69,28 +70,28 @@ const MasajBodyBuilding =() => {
                     }}
                 >
                  <Row justify="start" dir="rtl">
-                    <Col span={24}  style={{ textAlign:'center' ,  fontSize:'15px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'15px', paddingRight:'0px'  }}>
+                    <Col span={24}  style={{ textAlign:'center' ,  fontSize:'13px' , color:'#170602' , fontFamily:'irsans', marginTop:'15px', paddingRight:'0px'  }}>
                     ماساژ   کل بدن  با بادکش    
                     </Col>
                  </Row>
                  <br />
                  <Row justify="start" dir="rtl">
-                    <Col span={7}  style={{ textAlign:'center' ,  fontSize:'13px' , color:'#bab4b5' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={7}  style={{ textAlign:'center' ,  fontSize:'12px' , color:'#bab4b5' ,  fontFamily:'irsans', marginTop:'10px', paddingRight:'0px'  }}>
                       مدت زمان ماساژ:   
                     </Col>
-                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'13px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'12px' , color:'#170602' ,  fontFamily:'irsans', marginTop:'10px', paddingRight:'0px'  }}>
                         40 دقیقه
                     </Col>
-                    <Col span={6}  style={{ textAlign:'center' ,  fontSize:'13px' , color:'#bab4b5' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={6}  style={{ textAlign:'center' ,  fontSize:'12px' , color:'#bab4b5' , fontFamily:'irsans', marginTop:'10px', paddingRight:'0px'  }}>
                         هزینه ماساژ :
                     </Col>
-                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'13px' , color:'#170602' ,  fontFamily:'tahoma', fontWeight:'600', marginTop:'10px', paddingRight:'0px'  }}>
+                    <Col span={5}  style={{ textAlign:'right' ,  fontSize:'12px' , color:'#170602' , fontFamily:'irsans', marginTop:'10px', paddingRight:'0px'  }}>
                         40000 تومان  
                     </Col>
                  </Row>
                 <Row>
                         <Col  span={24}  style={{ textAlign:'center' , marginTop:'20px', marginBottom:'15px' }}>
-                            <Button type="primary" block   style={{  fontSize:'15px' , fontWeight:'600', height:'35px' ,   width:'90%' , background:'green' ,    }}>
+                            <Button type="primary" block   style={{  fontSize:'13px' ,fontFamily:'irsans', height:'35px' ,   width:'90%' , background:'green' ,    }}>
                                 رزرو  ماساژ
                             </Button>
                         </Col>
