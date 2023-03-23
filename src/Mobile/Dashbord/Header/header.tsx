d0208047ac479bc9078e08e295601443797086fd
@mohamad66 <rahimi@homacall.com>
2023-03-23 18:09:42 +0430

    t

--- src/Mobile/Dashbord/Header/header.tsx
+++ src/Mobile/Dashbord/Header/header.tsx
@@ -27,12 +27,12 @@ const Header =() => {
     
     return (
         <>
-          <Drawer title="کاسپین آبی" style={{ fontFamily:'tahoma', fontSize:'13px'}} bodyStyle={{textAlign:'right', fontFamily:'tahoma', fontSize:'12px'}} headerStyle={{textAlign:'right'}} width={200} placement="right" onClose={onClose} open={open}>
-                <p style={{ fontFamily:'tahoma', fontSize:'13px'}}>پروفایل</p>
-                <p style={{ fontFamily:'tahoma', fontSize:'13px'}}> <span style={{marginRight:'7.5%'}}><Badge size={"small"} count={badgeMessage} /></span>  پیام ها   </p>
-                <p style={{ fontFamily:'tahoma', fontSize:'13px'}} ><span style={{marginRight:'5%'}}><Badge size={"small"} count={badgeAlert} /></span> اعلانات </p>
-                <p style={{ fontFamily:'tahoma', fontSize:'13px'}}>مشاهده تراکنش ها</p>
-                <p style={{ fontFamily:'tahoma', fontSize:'13px'}}>درباره کاسپین ابی</p>
+          <Drawer title="کاسپین آبی" style={{ fontFamily:'irsans', fontSize:'15px'}} bodyStyle={{textAlign:'right', fontFamily:'irsans', fontSize:'15px'}} headerStyle={{textAlign:'right'}} width={200} placement="right" onClose={onClose} open={open}>
+                <p style={{ fontFamily:'irsans', fontSize:'14px'}}>پروفایل</p>
+                <p style={{ fontFamily:'irsans', fontSize:'14px'}}> <span style={{marginRight:'7.5%'}}><Badge size={"small"} count={badgeMessage} /></span>  پیام ها   </p>
+                <p style={{ fontFamily:'irsans', fontSize:'14px'}} ><span style={{marginRight:'5%'}}><Badge size={"small"} count={badgeAlert} /></span> اعلانات </p>
+                <p style={{ fontFamily:'irsans', fontSize:'14px'}}>مشاهده تراکنش ها</p>
+                <p style={{ fontFamily:'irsans', fontSize:'14px'}}>درباره کاسپین ابی</p>
          </Drawer>
          <div className="Header">
 
