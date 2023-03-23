313a2441ea79849bcf9bb1232785586be559bd3b
@mohamad66 <rahimi@homacall.com>
2023-03-23 22:41:47 +0430

    uu

--- src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
+++ src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
@@ -15,56 +15,56 @@ const  BodyBuilding =() =>{
 
            <div style={{marginRight:'2%' , marginLeft:'2%',  marginTop:'55px', }}>
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-                <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans',fontSize:'12px' }}>
+                <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans',fontSize:'13px' }}>
                   12 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'12px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
                  400,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
-                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' , fontFamily:'irsans', background:'green' ,   fontSize:'12px'}}>
+                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' , fontFamily:'irsans', background:'green' ,   fontSize:'11px'}}>
                      خرید اشتراک
                 </Button>
                 </Col>
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'12px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
                   16 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'12px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
                  500,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
-                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'12px'}}>
+                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'11px'}}>
                      خرید اشتراک
                 </Button>
                 </Col>
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'12px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
                   20 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'12px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px'}}>
                  600,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
-                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'12px'}}>
+                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'11px'}}>
                      خرید اشتراک
                 </Button>
                 </Col>
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'12px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px'}}>
                   30 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'12px' }}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px' }}>
                  800,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
-                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'12px'}}>
+                 <Button type="primary"    style={{ height:'30px' ,   width:'80%' ,  fontFamily:'irsans',background:'green' ,   fontSize:'11px'}}>
                      خرید اشتراک
                 </Button>
                 </Col>
