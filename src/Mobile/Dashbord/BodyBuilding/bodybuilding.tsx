8eef753614902777dd4fc8f639fa69cc800a2eb1
@mohamad66 <rahimi@homacall.com>
2023-03-23 22:54:52 +0430

    r

--- src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
+++ src/Mobile/Dashbord/BodyBuilding/bodybuilding.tsx
@@ -15,10 +15,10 @@ const  BodyBuilding =() =>{
 
            <div style={{marginRight:'2%' , marginLeft:'2%',  marginTop:'55px', }}>
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-                <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans',fontSize:'13px' }}>
+                <Col span={7}  style={{ textAlign:'center' , marginTop:'17px',  fontFamily:'irsans',fontSize:'13px' }}>
                   12 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'17px',  fontFamily:'irsans', fontSize:'13px'}}>
                  400,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
@@ -29,10 +29,10 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'17px',  fontFamily:'irsans', fontSize:'13px'}}>
                   16 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'17px',  fontFamily:'irsans', fontSize:'13px'}}>
                  500,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
@@ -43,10 +43,10 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',  fontFamily:'irsans', fontSize:'13px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'17px',  fontFamily:'irsans', fontSize:'13px'}}>
                   20 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px'}}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'17px',   fontFamily:'irsans', fontSize:'13px'}}>
                  600,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
@@ -57,10 +57,10 @@ const  BodyBuilding =() =>{
              </Row>
              <br />
              <Row justify="start" dir="rtl" style={{border:'1px dashed #cccccc ' ,height:'55px' , borderRadius:'5px'}}>
-             <Col span={7}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px'}}>
+             <Col span={7}  style={{ textAlign:'center' , marginTop:'17px',   fontFamily:'irsans', fontSize:'13px'}}>
                   30 جلسه در ماه
                 </Col>
-                <Col  span={9}  style={{ textAlign:'center' , marginTop:'15px',   fontFamily:'irsans', fontSize:'13px' }}>
+                <Col  span={9}  style={{ textAlign:'center' , marginTop:'17px',   fontFamily:'irsans', fontSize:'13px' }}>
                  800,000 تومان
                 </Col>
                 <Col  span={8}  style={{ textAlign:'center' , marginTop:'10px',  }}>
